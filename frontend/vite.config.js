f4a5490c78f694b695b8322da71b7444d47de495
Renata Ilyasheva <rena.abi14@gmail.com>
2025-12-12 19:02:27 +0300

    test fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://frontend-chat-ru.hexlet.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api'), 
