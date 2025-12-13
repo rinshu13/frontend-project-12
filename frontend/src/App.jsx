ad0c3a8f6ec618c4723ba2fba679d0e2d3ec662c
Renata Ilyasheva <rena.abi14@gmail.com>
2025-12-13 23:51:03 +0300

    fix3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -384,7 +384,7 @@ const App = () => {
                       <button
                         type="button"
                         className="dropdown-toggle"
-                        aria-label={t('dropdown.manageChannel')}  // "Управление каналом"
+                        aria-label={t('Управление каналом')}
                         onClick={(e) => e.stopPropagation()}
                       >
                         ⋮
