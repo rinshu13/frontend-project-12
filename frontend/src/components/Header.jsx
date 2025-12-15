d31760e1c739b29b6000501551a1f29905aa7371
Renata Ilyasheva <rena.abi14@gmail.com>
2025-12-15 19:23:41 +0300

    h

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,39 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
-import { logout } from '../features/auth/authSlice'
-import './Components.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { logout } from '../features/auth/authSlice';
 
 export const Header = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const { token } = useSelector(state => state.auth)
-  const { t } = useTranslation()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
+  const { t } = useTranslation();
 
   const handleLogout = () => {
-    dispatch(logout())
-    navigate('/login')
-  }
+    dispatch(logout());
+    navigate('/login');
+  };
 
   return (
-    <header className="app-header">
-      <div className="header-container">
-        <Link to="/" className="header-brand">
-          {t('header.title')}
-        </Link>
-        {token && (
-          <button className="header-logout-btn" onClick={handleLogout}>
-            {t('header.logout')}
-          </button>
-        )}
-      </div>
+    <header className="shadow-sm">
+      <nav className="navbar navbar-expand navbar-light bg-white py-3">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand fw-bold fs-4">
+            {t('header.title')}
+          </Link>
+
+          {token && (
+            <button
+              type="button"
+              className="btn btn-outline-danger"
+              onClick={handleLogout}
+            >
+              {t('header.logout')}
+            </button>
+          )}
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
